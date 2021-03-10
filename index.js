8749e7332e1b61250ef1929f1f5528d00b2318b4
Danielle <danisteele1984@gmail.com>
2021-03-10 14:27:38 +1100

    deployED

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
