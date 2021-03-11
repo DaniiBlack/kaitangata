b07c5d81341769ce9861b1c22adde00631f36c32
Danielle <danisteele1984@gmail.com>
2021-03-11 18:40:18 +1100

    ugh

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/resist.jpg" class="resist">
-      <nav>
-        <ul class="nav">
-          <li class="navItem"><router-link to="/">Home</router-link></li>
-          <li class="navItem"><router-link to="gallery">Gallery</router-link></li>
+    <img src="./assets/resist.jpg" class="resist"> 
+    <img src="./assets/forDaniLoveDan.png" class="lgbtqi">
+    <nav>
+      <ul class="nav">
+        <li class="navItem"><router-link to="/">Home</router-link></li>
+        <li class="navItem"><router-link to="gallery">Gallery</router-link></li>
       </ul>
     </nav>
     <h1 class="wd">Woke Dumplings</h1>
@@ -26,19 +27,25 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #f4f6f8;
-  margin-top: 60px;
+  margin-top: 15px;
 }
 body {
     background-color: black;
 }
 .nav {
   list-style-type: none;
-  margin-left: 100px;
+  margin-left: 60px;
+  position: relative;
+  z-index: 3;
 }
 .navItem {
   display: inline;
   padding: 5px;
   float: left;
+  color: white; 
+}
+a {
+  color: inherit;
 }
 .wd {
   clear: both;
@@ -49,5 +56,14 @@ body {
   position: absolute;
   left: 0px;
   top: -20px;
+  z-index: 2;
+}
+.lgbtqi {
+  width: 100%;
+  height: 30px;
+  position: absolute;
+  left: 0px;
+  z-index: 1;
+  opacity: 0.5;
 }
 </style>
